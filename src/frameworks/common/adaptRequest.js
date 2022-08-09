9037a374b451bbd112d136517201aadcee5ea645
hossein <derakhshanfar.hossein@gmail.com>
2022-08-09 23:19:48 +0300

    feat: add adaptRequest

---
+++ src/frameworks/common/adaptRequest.js
@@ -0,0 +1,9 @@
+module.exports = (req = {}) => {
+  return Object.freeze({
+    path: req.path,
+    method: req.method,
+    pathParams: req.params,
+    queryParams: req.query,
+    body: req.body,
+  })
+}
